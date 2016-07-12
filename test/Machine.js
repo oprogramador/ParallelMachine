effa9e7fc83b456893756e2b400d28670732152d
oprogramador <uomodislesia@gmail.com>
2016-07-13 01:22:13 +0200

    tests

---
+++ test/Machine.js
@@ -0,0 +1,9 @@
+const Machine = require('../src/Machine.js');
+const expect = require('chai').expect;
+
+describe('Machine', () => {
+  it('is a function', () => {
+    expect(typeof Machine).to.equal('function');
+  });
+});
+
